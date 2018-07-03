051b9e8f83e5bc76daabb477ad2d4b1d5b16e272
leedrake5 <b.lee.drake@gmail.com>
2018-07-03 13:06:32 -0600

    Equations
    Fixed equation models so that coencation c() is not printed

--- global.R
+++ global.R
@@ -538,8 +538,8 @@ lm_eqn.old <- function(df){
 
 lm_eqn = function(m) {
     
-    l <- list(a = format(coef(m)[1], digits = 2),
-    b = format(abs(coef(m)[2]), digits = 2),
+    l <- list(a = as.numeric(format(coef(m)[1], digits = 2)),
+    b = as.numeric(format(abs(coef(m)[2]), digits = 2)),
     r2 = format(summary(m)$r.squared, digits = 3));
     
         eq <- substitute(italic(C)[i] == a + b %.% italic(I)[i]*","~~italic(r)^2~"="~r2,l)
@@ -550,9 +550,9 @@ lm_eqn = function(m) {
 
 lm_eqn_poly = function(m) {
     
-    l <- list(a = format(coef(m)[1], digits = 2),
-    b = format(abs(coef(m)[2]), digits = 2),
-    c = format(abs(coef(m)[3]), digits = 2),
+    l <- list(a = as.numeric(format(coef(m)[1], digits = 2)),
+    b = as.numeric(format(abs(coef(m)[2]), digits = 2)),
+    c = as.numeric(format(abs(coef(m)[3]), digits = 2)),
     r2 = format(summary(m)$r.squared, digits = 3));
     
         eq <- substitute(italic(C)[i] == a + c %.% italic(I)[i]^2 + b %.% italic(I)[i]*","~~italic(r)^2~"="~r2,l)
@@ -563,8 +563,8 @@ lm_eqn_poly = function(m) {
 
 lm_eqn_val = function(m) {
     
-    l <- list(a = format(coef(m)[1], digits = 2),
-    b = format(abs(coef(m)[2]), digits = 2),
+    l <- list(a = as.numeric(format(coef(m)[1], digits = 2)),
+    b = as.numeric(format(abs(coef(m)[2]), digits = 2)),
     r2 = format(summary(m)$r.squared, digits = 3));
     
         eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
