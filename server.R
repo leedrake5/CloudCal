a54c4f01a2ed02f1817061ff5311e329f81f93c5
Lee Drake <b.lee.drake@gmail.com>
2019-07-09 19:23:39 -0600

    XGBoost Testing 10

--- server.R
+++ server.R
@@ -3427,7 +3427,7 @@ shinyServer(function(input, output, session) {
                 stopCluster(cl)
             } else if(get_os()=="linux"){
                 registerDoMC(as.numeric(my.cores))
-                caret::train(Concentration~., data=data[,-1], trControl = tune_control, tuneGrid = xgbGrid, metric=parameters$ForestMetric, method = "xgbTree", na.action=na.omit, nthread=as.numeric(my.cores))
+                caret::train(Concentration~., data=data[,-1], trControl = tune_control, tuneGrid = xgbGrid, metric=parameters$ForestMetric, method = "xgbTree", na.action=na.omit)
             }
             
             
