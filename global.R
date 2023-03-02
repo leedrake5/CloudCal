5827f0aaac3b6f02cc7df03d87a3af10fdb79cab
Lee Drake <b.lee.drake@gmail.com>
2023-03-02 13:46:28 -0700

    File Loading
    Reverting to loading from github on default, with local file as a tryCatch

--- global.R
+++ global.R
@@ -175,7 +175,7 @@ my.cores <- if(parallel::detectCores()>=3){
     "1"
 }
 
-source('file_loading.R')
+tryCatch(source("https://raw.githubusercontent.com/leedrake5/CloudCal/master/file_loading.R"), error=function(e) source('file_loading.R'))
 
 remove.factors = function(df) {
     for(varnum in 1:length(df)) {
