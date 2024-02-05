a728ff356c73c8046aa988397e0a2ee37f384570
Lee Drake <b.lee.drake@gmail.com>
2024-02-05 15:33:20 -0700

    Bayesian debug
    Added a Bayesian optimization debug  function. Also helper parallelization functions have been included.

--- global.R
+++ global.R
@@ -211,6 +211,11 @@ library(shinycssloaders)
 
 options(digits=12)
 
+unregister_dopar <- function() {
+  env <- foreach:::.foreachGlobals
+  rm(list=ls(name=env), pos=env)
+}
+
 my.cores <- if(parallel::detectCores()>=3){
     paste0(parallel::detectCores()-2)
 } else if(parallel::detectCores()<=2){
@@ -400,6 +405,140 @@ BayesianOptimization <- function(FUN, bounds, init_grid_dt = NULL, init_points =
         Next_Log <- tryCatch(utils::capture.output({
             Next_Time <- system.time({
                 Next_Score_Pred <- tryCatch(do.call(what = FUN, args = as.list(Next_Par)), error=function(e) list(Score=sample(-200:-150, 1)))
+            })
+        }), error=function(e) NULL)
+        tryCatch(data.table::set(DT_history, i = as.integer(j), j = c(DT_bounds[,
+            Parameter], "Value"), value = as.list(c(Next_Par,
+            Value = Next_Score_Pred$Score))), error=function(e) NULL)
+        tryCatch(Pred_list[[j]] <- Next_Score_Pred$Pred, error=function(e) NULL)
+        if (verbose == TRUE) {
+            tryCatch(paste(c("elapsed", names(DT_history)), c(format(Next_Time["elapsed"],
+                trim = FALSE, digits = NULL, nsmall = 2), format(DT_history[j,
+                "Round", with = FALSE], trim = FALSE, digits = NULL,
+                nsmall = 0), format(DT_history[j, -"Round", with = FALSE],
+                trim = FALSE, digits = NULL, nsmall = 4)), sep = " = ",
+                collapse = "\t") %>% cat(., "\n"), error=function(e) NULL)
+        }#, error=function(e) NULL})
+    }
+    Best_Par <- as.numeric(DT_history[which.max(Value), DT_bounds[,
+        Parameter], with = FALSE]) %>% magrittr::set_names(.,
+        DT_bounds[, Parameter])
+    Best_Value <- max(DT_history[, Value], na.rm = TRUE)
+    Pred_DT <- data.table::as.data.table(Pred_list)
+    Result <- list(Best_Par = Best_Par, Best_Value = Best_Value,
+        History = DT_history, Pred = Pred_DT)
+    cat("\n Best Parameters Found: \n")
+    paste(names(DT_history), c(format(DT_history[which.max(Value),
+        "Round", with = FALSE], trim = FALSE, digits = NULL,
+        nsmall = 0), format(DT_history[which.max(Value), -"Round",
+        with = FALSE], trim = FALSE, digits = NULL, nsmall = 4)),
+        sep = " = ", collapse = "\t") %>% cat(., "\n")
+    return(Result)
+}
+
+BayesianOptimizationDebug <- function(FUN, bounds, init_grid_dt = NULL, init_points = 0,
+    n_iter, acq = "ei", kappa = 2.576, eps = 0, kernel = list(type = "exponential",
+        power = 2), verbose = TRUE)
+{
+    DT_bounds <- data.table(Parameter = names(bounds), Lower = sapply(bounds,
+    magrittr::extract2, 1), Upper = sapply(bounds, magrittr::extract2, 2), Type = sapply(bounds,
+        class))
+    setDT(init_grid_dt)
+    if (nrow(init_grid_dt) != 0) {
+        if (identical(names(init_grid_dt), DT_bounds[, Parameter]) ==
+            TRUE) {
+            init_grid_dt[, `:=`(Value, -Inf)]
+        }
+        else if (identical(names(init_grid_dt), c(DT_bounds[,
+            Parameter], "Value")) == TRUE) {
+            paste(nrow(init_grid_dt), "points in hyperparameter space were pre-sampled\n",
+                sep = " ") %>% cat(.)
+        }
+        else {
+            stop("bounds and init_grid_dt should be compatible")
+        }
+    }
+    init_points_dt <- Matrix_runif(n = init_points, lower = DT_bounds[,
+        Lower], upper = DT_bounds[, Upper]) %>% data.table(.) %T>%
+        setnames(., old = names(.), new = DT_bounds[, Parameter]) %T>%
+        {
+            if (any(DT_bounds[, Type] == "integer")) {
+                set(., j = DT_bounds[Type == "integer", Parameter],
+                  value = round(extract(., j = DT_bounds[Type ==
+                    "integer", Parameter], with = FALSE)))
+            }
+            else {
+                .
+            }
+        } %T>% extract(., j = `:=`(Value, -Inf))
+    iter_points_dt_backup <- Matrix_runif(n = init_points+n_iter, lower = DT_bounds[,
+            Lower], upper = DT_bounds[, Upper]) %>% data.table(.) %T>%
+            setnames(., old = names(.), new = DT_bounds[, Parameter]) %T>%
+            {
+                if (any(DT_bounds[, Type] == "integer")) {
+                    set(., j = DT_bounds[Type == "integer", Parameter],
+                      value = round(extract(., j = DT_bounds[Type ==
+                        "integer", Parameter], with = FALSE)))
+                }
+                else {
+                    .
+                }
+            }
+    iter_points_dt <- data.table(matrix(-Inf, nrow = n_iter,
+        ncol = nrow(DT_bounds) + 1)) %>% setnames(., old = names(.),
+        new = c(DT_bounds[, Parameter], "Value"))
+    DT_history <- rbind(init_grid_dt, init_points_dt, iter_points_dt) %>%
+        cbind(data.table(Round = 1:nrow(.)), .)
+    Pred_list <- vector(mode = "list", length = nrow(DT_history))
+    for (i in 1:(nrow(init_grid_dt) + nrow(init_points_dt))) {
+        if (is.infinite(DT_history[i, Value]) == TRUE) {
+            This_Par <- DT_history[i, DT_bounds[, Parameter],
+                with = FALSE]
+        }
+        else {
+            next
+        }
+        This_Log <- utils::capture.output({
+            This_Time <- system.time({
+                This_Score_Pred <- do.call(what = FUN, args = as.list(This_Par))
+            })
+        })
+        data.table::set(DT_history, i = as.integer(i), j = "Value",
+            value = as.list(c(This_Score_Pred$Score)))
+        Pred_list[[i]] <- This_Score_Pred$Pred
+        if (verbose == TRUE) {
+            paste(c("elapsed", names(DT_history)), c(format(This_Time["elapsed"],
+                trim = FALSE, digits = 3, nsmall = 2), format(DT_history[i,
+                "Round", with = FALSE], trim = FALSE, digits = NULL,
+                nsmall = 0), format(DT_history[i, -"Round", with = FALSE],
+                trim = FALSE, digits = 3, nsmall = 3)), sep = " = ",
+                collapse = "\t") %>% cat(., "\n")
+        }
+    }
+   for (j in (nrow(init_grid_dt) + nrow(init_points_dt) + 1):nrow(DT_history)) {
+       if (nrow(iter_points_dt) == 0) {
+            next
+        }
+        Par_Mat <- Min_Max_Scale_Mat(as.matrix(DT_history[1:(j -
+            1), DT_bounds[, Parameter], with = FALSE]), lower = DT_bounds[,
+            Lower], upper = DT_bounds[, Upper])
+        Rounds_Unique <- setdiff(1:(j - 1), which(duplicated(Par_Mat) ==
+            TRUE))
+        Value_Vec <- DT_history[1:(j - 1), Value]
+        GP_Log <- utils::capture.output({
+            GP <- GPfit::GP_fit(X = Par_Mat[Rounds_Unique, ],
+                Y = Value_Vec[Rounds_Unique], corr = kernel)
+        })
+        Next_Par <- tryCatch(Utility_Max(DT_bounds, GP, acq = acq, y_max = max(DT_history[,
+            Value]), kappa = kappa, eps = eps) %>% Min_Max_Inverse_Scale_Vec(.,
+            lower = DT_bounds[, Lower], upper = DT_bounds[, Upper]) %>%
+            magrittr::set_names(., DT_bounds[, Parameter]) %>%
+            inset(., DT_bounds[Type == "integer", Parameter],
+                round(extract(., DT_bounds[Type == "integer",
+                  Parameter]))), error=function(e) unlist(iter_points_dt_backup[j,]))
+        Next_Log <- tryCatch(utils::capture.output({
+            Next_Time <- system.time({
+                Next_Score_Pred <- do.call(what = FUN, args = as.list(Next_Par))
             })
         }), error=function(e) NULL)
         tryCatch(data.table::set(DT_history, i = as.integer(j), j = c(DT_bounds[,
