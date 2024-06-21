3df4af381ddd5edcb54e7e0abe1f3d1ce5fa0b71
Lee Drake <b.lee.drake@gmail.com>
2024-06-21 16:16:37 -0600

    Reference change

--- global.R
+++ global.R
@@ -219,7 +219,7 @@ my.cores <- if(parallel::detectCores()>=3){
 }
 
 #source('file_loading.R')
-tryCatch(source('file_loading.R'), error=function(e) source("https://raw.githubusercontent.com/leedrake5/CloudCal/master/file_loading.R"))
+tryCatch(source('file_loading.R'), error=function(e) source("https://raw.githubusercontent.com/leedrake5/CloudCal/line_calculation/file_loading.R"))
 
 remove.factors = function(df) {
     for(varnum in 1:length(df)) {
