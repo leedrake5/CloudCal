20808ee2643f009b92adb59c35796847d9797dba
Lee Drake <b.lee.drake@gmail.com>
2026-04-04 20:16:16 -0700

    rPDZ update

--- global.R
+++ global.R
@@ -89,15 +89,15 @@ if("caret" %in% installed.packages()[,"Package"]==FALSE && get_os()=="windows"){
 #    }
 
 if("rPDZ" %in% installed.packages()[,"Package"]==FALSE && get_os()=="windows"){
-        tryCatch(install.packages("https://github.com/leedrake5/CloudCal/raw/line_calculation/Packages/rPDZ_2.0.zip", repos=NULL, type="win.binary"), error=function(e) tryCatch(remotes::install_github("leedrake5/rPDZ"), error=function(e) NULL))
+        tryCatch(install.packages("https://github.com/leedrake5/CloudCal/raw/line_calculation/Packages/rPDZ_2.0.1.zip", repos=NULL, type="win.binary"), error=function(e) tryCatch(remotes::install_github("leedrake5/rPDZ"), error=function(e) NULL))
     } else if("rPDZ" %in% installed.packages()[,"Package"]==FALSE && get_os()!="windows"){
-        tryCatch(install.packages("https://github.com/leedrake5/CloudCal/raw/line_calculation/Packages/rPDZ_2.0.tar.gz", type="source", repos=NULL), error=function(e) tryCatch(remotes::install_github("leedrake5/rPDZ"), error=function(e) NULL))
+        tryCatch(install.packages("https://github.com/leedrake5/CloudCal/raw/line_calculation/Packages/rPDZ_2.0.1.tar.gz", type="source", repos=NULL), error=function(e) tryCatch(remotes::install_github("leedrake5/rPDZ"), error=function(e) NULL))
     }
 
-if(packageVersion("rPDZ")!="2.0" && get_os()=="windows"){
-    tryCatch(install.packages("https://github.com/leedrake5/CloudCal/raw/line_calculation/Packages/rPDZ_2.0.zip", repos=NULL, type="win.binary"), error=function(e) tryCatch(remotes::install_github("leedrake5/rPDZ"), error=function(e) NULL))
-} else if(packageVersion("rPDZ")!="2.0" && get_os()!="windows"){
-    tryCatch(install.packages("https://github.com/leedrake5/CloudCal/raw/line_calculation/Packages/rPDZ_2.0.tar.gz", type="source", repos=NULL), error=function(e) tryCatch(remotes::install_github("leedrake5/rPDZ"), error=function(e) NULL))
+if(packageVersion("rPDZ")!="2.0.1" && get_os()=="windows"){
+    tryCatch(install.packages("https://github.com/leedrake5/CloudCal/raw/line_calculation/Packages/rPDZ_2.0.1.zip", repos=NULL, type="win.binary"), error=function(e) tryCatch(remotes::install_github("leedrake5/rPDZ"), error=function(e) NULL))
+} else if(packageVersion("rPDZ")!="2.0.1" && get_os()!="windows"){
+    tryCatch(install.packages("https://github.com/leedrake5/CloudCal/raw/line_calculation/Packages/rPDZ_2.0.1.tar.gz", type="source", repos=NULL), error=function(e) tryCatch(remotes::install_github("leedrake5/rPDZ"), error=function(e) NULL))
 }
 
 
